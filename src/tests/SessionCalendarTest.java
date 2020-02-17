6c3ece134614cf42fd582e09b31ecd04647dead1
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-02-17 14:15:47 +0100

    Test package aangemaakt en voorbeeldtest gemaakt

---
+++ src/tests/SessionCalendarTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SessionCalendarTest {
+	
+
+    @Test
+    void addition() {
+        assertEquals(2, 1+1);
+    }
+	
+
+}
