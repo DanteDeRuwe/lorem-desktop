0cefa4802f3639abc1f1177b4d667c65790dd316
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-02-23 22:40:55 +0100

    Create FeedbackEntryTest

---
+++ src/tests/FeedbackEntryTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import domain.FeedbackEntry;
+import domain.Member;
+import domain.MemberType;
+
+class FeedbackEntryTest {
+
+	static Member exampleAuthor;
+	static String exampleText, exampleTitle;
+
+	@BeforeAll
+	static void beforeAll() {
+		exampleAuthor = new Member("johnsmith", "John", "Smith", MemberType.USER);
+		exampleText = "Lorem ipsum dolor sit amet";
+		exampleTitle = "Lorem ipsum";
+	}
+
+	@ParameterizedTest
+	@NullAndEmptySource
+	void createFeedbackEntry_EmptyOrNullTitle_ThrowIllegalArgumentException(String title) {
+		assertThrows(IllegalArgumentException.class, () -> new FeedbackEntry(exampleAuthor, title, exampleText));
+	}
+	
+	@ParameterizedTest
+	@NullAndEmptySource
+	void createFeedbackEntry_EmptyOrNullText_ThrowIllegalArgumentException(String text) {
+		assertThrows(IllegalArgumentException.class, () -> new FeedbackEntry(exampleAuthor, exampleTitle, text));
+	}
+
+}
