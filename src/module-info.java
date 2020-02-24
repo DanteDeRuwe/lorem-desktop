27c1458c85bca218c9232e791c37fec83645ddc0
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-02-24 13:06:09 +0100

    Moved module-info to correct location

--- src/module-info.java
+++ src/module-info.java
@@ -3,11 +3,12 @@ open module JpaEnJavaFx {
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
-	
+
 	requires java.persistence;
 	requires java.instrument;
 	requires java.sql;
 	requires org.junit.jupiter.api;
 	requires mockito.junit.jupiter;
 	requires org.mockito;
+	requires org.junit.jupiter.params;
 }
