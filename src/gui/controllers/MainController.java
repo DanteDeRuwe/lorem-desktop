c5d95bd325c8baedcfe43a32baf9c8307fd27aba
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-03 11:43:36 +0100

    Remove search functionality

--- src/gui/controllers/MainController.java
+++ src/gui/controllers/MainController.java
@@ -14,29 +14,10 @@ public class MainController {
 	private AnchorPane main;
 
 	@FXML
-	private TextField searchBox;
-
-	@FXML
-	private Button searchButton;
-
-	@FXML
 	private SessionController sessionController;
 
 	@FXML
 	public void initialize() {
-		searchBox.textProperty().addListener((observable, oldValue, newValue) -> performSearch(newValue));
-		searchBox.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler -> searchBox.setText(""));
-	}
-
-	private void performSearch(String q) {
-		String query = q.toLowerCase();
-		TableView<Session> sessionTable = sessionController.getSessionTable();
-
-		sessionTable.getItems().stream().filter(item -> item.getTitle().toLowerCase().contains(query)).findAny()
-				.ifPresent(item -> {
-					sessionTable.getSelectionModel().select(item);
-					sessionTable.scrollTo(item);
-				});
 	}
 
 }
