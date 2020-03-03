054722b53b7d5beed38d3467a8d3198f993a207c
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-03-03 15:04:55 +0100

    Updated module info

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+open module javag09 {
+	exports main.domain;
+	exports test.domain;
+	exports main.services;
+	exports gui.controllers;
+	exports main;
+	
+
+	requires javafx.fxml;
+	requires javafx.base;
+	requires javafx.controls;
+	requires transitive javafx.graphics;
+	
+
+	requires java.persistence;
+	requires java.instrument;
+	requires java.sql;
+	requires org.junit.jupiter.api;
+	requires mockito.junit.jupiter;
+	requires org.mockito;
+	requires org.junit.jupiter.params;
+
+}
