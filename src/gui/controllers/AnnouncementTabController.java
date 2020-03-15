a9fdf722d9f40c67bd8e8a9afd1958a9a18763ec
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-15 17:10:09 +0100

    Add announcement tab

--- src/gui/controllers/AnnouncementTabController.java
+++ src/gui/controllers/AnnouncementTabController.java
@@ -1,12 +1,26 @@
 package gui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import main.domain.Session;
 
 public class AnnouncementTabController extends GuiController {
 
 	@FXML
+	private Button addAnnouncementButton, modifyAnnouncementButton, deleteAnnouncementButton;
+	@FXML
+	private ListView announcementListView;
+
+	private Session inspectedSession;
+
+	@FXML
 	public void initialize() {
-		
 	}
-	
+
+	public void setInspectedSession(Session session) {
+		inspectedSession = session;
+		// change listview source
+	}
+
 }
