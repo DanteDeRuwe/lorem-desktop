346ecc910722767b155fe690fa08a17e2289dafe
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-15 14:23:14 +0100

    Add announcement tab to UI

---
+++ src/gui/controllers/AnnouncementTabController.java
@@ -0,0 +1,12 @@
+package gui.controllers;
+
+import javafx.fxml.FXML;
+
+public class AnnouncementTabController extends GuiController {
+
+	@FXML
+	public void initialize() {
+		
+	}
+	
+}
--- src/gui/controllers/SessionTabsController.java
+++ src/gui/controllers/SessionTabsController.java
@@ -7,17 +7,21 @@ import main.services.GuiUtil;
 
 public class SessionTabsController extends GuiController {
 
-	@FXML private AnchorPane infoTab;
+	@FXML private AnchorPane infoTab, announcementTab;
 
 	private InfoTabController infoTabController;
+	private AnnouncementTabController announcementTabController;
 
 	@FXML
 	public void initialize() {
 		infoTabController = new InfoTabController();
-
+		announcementTabController = new AnnouncementTabController();
+		
 		AnchorPane infoTabRoot = loadFXML("sessions/tabs/InfoTab.fxml", infoTabController, getFacade());
-
+		AnchorPane announcementTabRoot = loadFXML("sessions/tabs/AnnouncementTab.fxml", announcementTabController, getFacade());
+		
 		GuiUtil.bindAnchorPane(infoTabRoot, infoTab);
+		GuiUtil.bindAnchorPane(announcementTabRoot, announcementTab);
 	}
 
 	public void showSessionInfo(Session session) {
