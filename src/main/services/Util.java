d7bb9517e46d507b8c1072bf75bf812be2dae960
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-03-15 17:13:24 +0100

    Cleaned up Util: unused methods

--- src/main/services/Util.java
+++ src/main/services/Util.java
@@ -1,8 +1,6 @@
 package main.services;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -17,34 +15,12 @@ public class Util {
 		return (one.getDayOfYear() == two.getDayOfYear() && one.getYear() == two.getYear());
 	}
 
-	public static LocalDateTime[] randomStartStop() {
-		LocalDateTime start = randomLocalDateTimeThisYearFromTomorrow();
-		LocalDateTime stop = start.plusHours(ThreadLocalRandom.current().nextInt(1, 8));
-		return new LocalDateTime[] { start, stop };
-	}
-
 	public static <T> T randomChoice(List<T> list) {
 		return list.get(ThreadLocalRandom.current().nextInt(0, list.size()));
 	}
 
 	public static <T> T randomChoice(T[] arr) {
 		return arr[ThreadLocalRandom.current().nextInt(0, arr.length)];
-	}
-
-	// helpers
-
-	private static LocalDateTime randomLocalDateTimeThisYearFromTomorrow() {
-		long minDay = LocalDate.now().plusDays(1).toEpochDay();
-		long maxDay = LocalDate.of(2020, 12, 31).toEpochDay();
-		long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
-		LocalDate randomDate = LocalDate.ofEpochDay(randomDay);
-
-		int randomHour = ThreadLocalRandom.current().nextInt(7, 20);
-		int randomMin = ThreadLocalRandom.current().nextInt(0, 59);
-
-		LocalTime randomTime = LocalTime.of(randomHour, randomMin);
-
-		return LocalDateTime.of(randomDate, randomTime);
 	}
 
 }
