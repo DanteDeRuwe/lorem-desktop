dec245aaf96fda9615a443bb4de46b3f2d5f07d2
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-16 18:50:33 +0100

    Implement first name, last name and username filters user scene

--- src/gui/controllers/UserFiltersController.java
+++ src/gui/controllers/UserFiltersController.java
@@ -1,25 +1,53 @@
 package gui.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
 import javafx.fxml.FXML;
+import main.domain.Member;
+import main.domain.facades.MemberFacade;
 
-public class UserFiltersController extends GuiController{
+public class UserFiltersController extends GuiController {
 
-    @FXML
-    private JFXButton newUserButton;
-    
-    @FXML
+	@FXML
+	private JFXButton newUserButton;
+	@FXML
+	private JFXTextField lastNameFilterField, firstNameFilterField, usernameFilterField;
+
+	@FXML
 	public void initialize() {
-
 		// Event Listeners
 		newUserButton.setOnAction(e -> handleNewUser());
 
-		// TODO filters
-		// titleFilter.textProperty().addListener((obs, oldText, newText) -> { });
-
+		// Filtering
+		lastNameFilterField.textProperty().addListener((obs, oldText, newText) -> {
+			getMainController().getUserSceneController().fillTableColumns(filter());
+		});
+		firstNameFilterField.textProperty().addListener((obs, oldText, newText) -> {
+			getMainController().getUserSceneController().fillTableColumns(filter());
+		});
+		usernameFilterField.textProperty().addListener((obs, oldText, newText) -> {
+			getMainController().getUserSceneController().fillTableColumns(filter());
+		});
 	}
-    
-    private void handleNewUser() {
+
+	private List<Member> filter() {
+		String lastNameFilter = lastNameFilterField.getText();
+		String firstNameFilter = firstNameFilterField.getText();
+		String usernameFilter = usernameFilterField.getText();
+		List<Member> memberList = new ArrayList<>(((MemberFacade) getFacade()).getAllMembers());
+		return memberList.stream()
+				.filter(m -> m.getLastName().toLowerCase().contains((lastNameFilter.toLowerCase().trim())))
+				.filter(m -> m.getFirstName().toLowerCase().contains((firstNameFilter.toLowerCase().trim())))
+				.filter(m -> m.getUsername().toLowerCase().contains((usernameFilter.toLowerCase().trim())))
+				.collect(Collectors.toList());
+	}
+
+	private void handleNewUser() {
 		((UserSceneController) getParentController()).displayOnRightPane("NewUser");
 	}
 
--- src/gui/controllers/UserSceneController.java
+++ src/gui/controllers/UserSceneController.java
@@ -1,87 +1,83 @@
 package gui.controllers;
 
+import java.util.List;
+
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import main.domain.Member;
-import main.domain.Session;
 import main.domain.facades.MemberFacade;
 import main.services.GuiUtil;
 
 public class UserSceneController extends GuiController {
-	
+
 	// Controllers
 	private GuiController userDetailsController, newUserController, userFiltersController;
-	
+
 	// Own vars
-	private ObservableList<Member> userList;
 	private AnchorPane userDetails, newUser, userFilters;
-	
+
 	// FXML vars
-	@FXML private AnchorPane leftPane, middlePane, rightPane;
-	@FXML protected TableView<Member> userTable;
-	@FXML private TableColumn<Member, String> lastNameColumn, firstNameColumn, usernameColumn;
-	@FXML private TableColumn<Member, Object> typeColumn, statusColumn;
-	
-	
-	
+	@FXML
+	private AnchorPane leftPane, middlePane, rightPane;
+	@FXML
+	protected TableView<Member> userTable;
+	@FXML
+	private TableColumn<Member, String> lastNameColumn, firstNameColumn, usernameColumn;
+	@FXML
+	private TableColumn<Member, Object> typeColumn, statusColumn;
+
 	/*
 	 * Init
 	 */
 
 	@FXML
 	public void initialize() {
-		
+
 		// initialize controllers
 		userDetailsController = new UserDetailsController();
 		newUserController = new NewUserController();
 		userFiltersController = new UserFiltersController();
-		
+
 		// load FXML once, this also sets parentcontrollers and facades
 		userDetails = loadFXML("users/UserDetails.fxml", userDetailsController, this.getFacade());
 		newUser = loadFXML("users/NewUser.fxml", newUserController, this.getFacade());
 		userFilters = loadFXML("users/UserFilters.fxml", userFiltersController, this.getFacade());
-		
 
 		// Center Panel
-		fillTableColumns();
-		
+		fillTableColumns(((MemberFacade) getFacade()).getAllMembers());
+
 		// Right panel
 		GuiUtil.bindAnchorPane(userDetails, rightPane);
-		
+
 		// Left Panel
 		GuiUtil.bindAnchorPane(userFilters, leftPane);
-		
-		// Event Handlers
-		userTable.getSelectionModel().selectedItemProperty().addListener(
-			(x, y, user) -> ((UserDetailsController) userDetailsController).setInspectedUser(user));
 
-		
-		
+		// Event Handlers
+		userTable.getSelectionModel().selectedItemProperty()
+				.addListener((x, y, user) -> ((UserDetailsController) userDetailsController).setInspectedUser(user));
+
 	}
-	
+
 	/*
 	 * Helpers
 	 */
-	
-	void fillTableColumns() {
-		userList = FXCollections.observableArrayList(((MemberFacade) getFacade()).getAllMembers());
-		
+
+	void fillTableColumns(List<Member> members) {
 		GuiUtil.fillColumn(lastNameColumn, "lastName", 40, 200);
 		GuiUtil.fillColumn(firstNameColumn, "firstName", 40, 200);
 		GuiUtil.fillColumn(usernameColumn, "username", 40, 200);
 		GuiUtil.fillColumnWithObjectToString(typeColumn, "memberType", 40, 200);
 		GuiUtil.fillColumnWithObjectToString(statusColumn, "memberStatus", 40, 200);
-		
-		userTable.setItems(userList);
+
+		userTable.setItems(FXCollections.observableArrayList(members));
 	}
-	
+
 	void update() {
 		// update the view
-		fillTableColumns();
+		fillTableColumns(((MemberFacade) getFacade()).getAllMembers());
 		userTable.getSelectionModel().selectFirst(); // select first user
 	}
 
@@ -98,5 +94,5 @@ public class UserSceneController extends GuiController {
 		else
 			throw new RuntimeException("key not valid");
 	}
-	
+
 }
