d3ce17bea6dcdcc71bd50fce8aab09b80cc28c1c
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-18 14:47:43 +0100

    When creating a calendar spanning more than 1 academic year, show alert
    Issue #15

--- src/gui/controllers/NewCalendarController.java
+++ src/gui/controllers/NewCalendarController.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import com.jfoenix.controls.JFXDatePicker;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import main.domain.SessionCalendar;
@@ -13,14 +15,17 @@ import main.services.GuiUtil;
 
 public class NewCalendarController extends GuiController {
 
-	@FXML private Label topLabel;
-	@FXML private JFXDatePicker startDatePicker, endDatePicker;
-	@FXML private Button saveButton, cancelButton;
+	@FXML
+	private Label topLabel;
+	@FXML
+	private JFXDatePicker startDatePicker, endDatePicker;
+	@FXML
+	private Button saveButton, cancelButton;
 
 	@FXML
 	public void initialize() {
 		topLabel.setText("Nieuwe Kalender");
-		
+
 		// Date picker format
 		GuiUtil.fixDatePicker(startDatePicker);
 		GuiUtil.fixDatePicker(endDatePicker);
@@ -31,18 +36,24 @@ public class NewCalendarController extends GuiController {
 	}
 
 	private void handleCreate() {
-
 		LocalDate start = startDatePicker.getValue();
 		LocalDate end = endDatePicker.getValue();
 
 		SessionCalendarFacade scf = ((SessionCalendarFacade) getFacade());
-
-		SessionCalendar sc = scf.createSessionCalendar(start, end);
-		scf.addSessionCalendar(sc);
-
-		getMainController().getCalendarSceneController().update();
-
-		goBack();
+		try {
+			SessionCalendar sc = scf.createSessionCalendar(start, end);
+			scf.addSessionCalendar(sc);
+			getMainController().getCalendarSceneController().update();
+			goBack();
+		} catch (IllegalArgumentException e) {
+			if (e.getMessage().equals("Academic years must start and end in consecutive years")) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Nieuwe kalender");
+				alert.setHeaderText("Fout");
+				alert.setContentText("Een academisch jaar moet starten en eindigen in opeenvolgende jaren");
+				alert.showAndWait();
+			}
+		}
 	}
 
 	private void goBack() {
