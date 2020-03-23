cb2b561600c4bc60751a27654b418db3e8143726
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-03-23 17:08:30 +0100

    Fixed issue #44

--- src/main/services/AnnouncementCellFactory.java
+++ src/main/services/AnnouncementCellFactory.java
@@ -17,21 +17,25 @@ public class AnnouncementCellFactory<T> implements Callback<ListView<Announcemen
 			@Override
 			protected void updateItem(Announcement item, boolean empty) {
 				super.updateItem(item, empty);
-				if (item != null) {
-					// create components with announcement data
-					Label titleLabel = new Label(item.getTitle());
-					Label timestampLabel = new Label(item.getTimestamp().format(Util.DATETIMEFORMATTER));
-					Label authorLabel = new Label(item.getAuthor().getFullName());
-					Text text = new Text(item.getText());
-
-					// add components to containers
-					HBox hbox = new HBox();
-					hbox.getChildren().addAll(authorLabel, new Label(" om "), timestampLabel);
-					VBox vbox = new VBox();
-					vbox.getChildren().addAll(titleLabel, hbox, text);
-					setGraphic(vbox);
+				if (item == null || empty) {
+					setGraphic(null);
+					setText(null);
+					return;
 				}
+				// create components with announcement data
+				Label titleLabel = new Label(item.getTitle());
+				Label timestampLabel = new Label(item.getTimestamp().format(Util.DATETIMEFORMATTER));
+				Label authorLabel = new Label(item.getAuthor().getFullName());
+				Text text = new Text(item.getText());
+
+				// add components to containers
+				HBox hbox = new HBox();
+				hbox.getChildren().addAll(authorLabel, new Label(" om "), timestampLabel);
+				VBox vbox = new VBox();
+				vbox.getChildren().addAll(titleLabel, hbox, text);
+				setGraphic(vbox);
 			}
+
 		};
 	}
 
