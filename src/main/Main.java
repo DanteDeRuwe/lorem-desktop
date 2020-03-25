e83a1b94c17d63e1b55b20d2ed128183f4b2d05e
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-03-25 20:06:04 +0100

    Add icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import persistence.GenericDaoJpa;
 
@@ -32,6 +33,8 @@ public class Main extends Application {
 		stage.setScene(scene);
 		stage.setTitle("Login");
 		stage.setResizable(false);
+		stage.getIcons().add(new Image(Main.class.getResourceAsStream("/resources/icon/itlab.png")));
+
 		
 		stage.show();
 
