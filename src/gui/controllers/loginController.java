4f59ccbd5b6385dbf2af7263f83e4579a48f860b
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-03-25 10:20:57 +0100

    added login screen, not yet accessible

---
+++ src/gui/controllers/loginController.java
@@ -0,0 +1,19 @@
+package gui.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class loginController {
+
+    @FXML
+    private JFXTextField usernameField;
+
+    @FXML
+    private JFXPasswordField passwordName;
+    
+    @FXML
+    private JFXButton loginButton;
+
+}
